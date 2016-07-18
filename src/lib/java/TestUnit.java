d8bd1f7dfb52a384f20ad8592b0aa87956bc6b7b
Yanir Taflev <yanir.taflev@applitools.com>
2016-07-18 11:37:13 +0300

    Adding readablily check to directories and files

--- src/lib/java/TestUnit.java
+++ src/lib/java/TestUnit.java
@@ -2,12 +2,16 @@ import com.applitools.eyes.Eyes;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
 
 public abstract class TestUnit {
     protected final File file_;
 
     protected TestUnit(File file) {
         file_ = file;
+        if (file != null && !file.canRead()) {
+            throw new RuntimeException(String.format("Unreadable path/file %s, might be a permission issue!", file.getAbsolutePath()));
+        }
     }
 
     public abstract void run(Eyes eyes) throws IOException;
