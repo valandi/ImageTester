6771388dee30e5df90ac653ba3d4f0b960e5d8f3
Yanir Taflev <yanirta@users.noreply.github.com>
2017-06-04 17:28:56 +0300

    Update Batch.java

--- src/lib/java/com/applitools/ImageTester/Batch.java
+++ src/lib/java/com/applitools/ImageTester/Batch.java
@@ -23,7 +23,7 @@ public class Batch extends TestUnit {
     public void run(Eyes eyes) {
         batch_ = batch_ == null ? new BatchInfo(name()) : batch_;
         eyes.setBatch(batch_);
-        System.out.printf("com.applitools.com.applitools.ImageTester.ImageTester.Batch: %s\n", name());
+        System.out.printf("Batch: %s\n", name());
         for (Test test : tests_) {
             test.run(eyes);
         }
